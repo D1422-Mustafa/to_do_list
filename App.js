7e9731d1a6e1c57162d0a8a55a334b5ab7f08f8b
Mustafa TAÇYILDIZ <93773148+D1422-Mustafa@users.noreply.github.com>
2022-02-18 17:57:16 +0300

    değişiklikler eklendi.

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ let plus = document.getElementById("plus");
 let taskTitle = document.getElementById("task__title");
 let taskList = document.getElementById("task__title");
 let todoList = document.getElementById("todo_list")
+let rules = document.getElementById("rules")
 
 plus.addEventListener("click", ()=>{
     taskTitle.style.display ="block"
@@ -16,11 +17,15 @@ plus.addEventListener("click", ()=>{
     todoList.append(remove);
     let brow =document.createElement("br");
     todoList.append(brow);
+    rules.style.display = "none";
     input.value = "";
 
     toDo.addEventListener("click", ()=>{
         toDo.style.textDecorationLine = "line-through";
         toDo.style.textDecorationColor = "red";
+    })
+    toDo.addEventListener("dblclick", ()=>{
+        toDo.style.textDecorationLine = "none";
     })
 
     remove.addEventListener("click", ()=>{
