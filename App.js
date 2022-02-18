74c96de90e09905cf714ed741f4ddc812e5a499f
Mustafa TAÇYILDIZ <93773148+D1422-Mustafa@users.noreply.github.com>
2022-02-18 18:44:33 +0300

    değişiklikler yapıldı

--- App.js
+++ App.js
@@ -7,35 +7,38 @@ let rules = document.getElementById("rules");
 input.focus();
 
 plus.addEventListener("click", ()=>{
-    taskTitle.style.display ="block"
-    let toDo = document.createElement("span");
-    toDo.setAttribute("id", "todo");
-    toDo.innerText = input.value;
-    todoList.append(toDo);
-    let remove = document.createElement("button");
-    remove.setAttribute("id", "remove");
-    remove.innerText = "X";
-    todoList.append(remove);
-    let brow =document.createElement("br");
-    todoList.append(brow);
-    rules.style.display = "none";
-    input.focus();
-    input.value = "";
+    if(input.value != ""){
+        taskTitle.style.display ="block"
+        let toDo = document.createElement("span");
+        toDo.setAttribute("id", "todo");
+        toDo.innerText = input.value;
+        todoList.append(toDo);
+        let remove = document.createElement("button");
+        remove.setAttribute("id", "remove");
+        remove.innerText = "X";
+        todoList.append(remove);
+        let brow =document.createElement("br");
+        todoList.append(brow);
+        rules.style.display = "none";
+        input.focus();
+        input.value = "";
 
-toDo.addEventListener("click", ()=>{
-    toDo.style.textDecorationLine = "line-through";
-    toDo.style.textDecorationColor = "red";
-})
-toDo.addEventListener("dblclick", ()=>{
-    toDo.style.textDecorationLine = "none";
-})
+    toDo.addEventListener("click", ()=>{
+        toDo.style.textDecorationLine = "line-through";
+        toDo.style.textDecorationColor = "red";
+    })
+    toDo.addEventListener("dblclick", ()=>{
+        toDo.style.textDecorationLine = "none";
+    })
 
-remove.addEventListener("click", ()=>{
-    remove.style.display = "none";
-    toDo.style.display = "none";
-    brow.style.display = "none";
-})
-
+    remove.addEventListener("click", ()=>{
+        remove.style.display = "none";
+        toDo.style.display = "none";
+        brow.style.display = "none";
+    })
+}else{
+    alert("please enter to do!")
+}
 })
 
 document.addEventListener("keyup", (e) => {
