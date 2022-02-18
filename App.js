23629a8bae7823e1e2aa71371d7bca7dd7bb58f8
Mustafa TAÇYILDIZ <93773148+D1422-Mustafa@users.noreply.github.com>
2022-02-18 17:30:41 +0300

    to do list çalışması yapıldı

---
+++ App.js
@@ -0,0 +1,30 @@
+let input = document.getElementById("input");
+let plus = document.getElementById("plus");
+let taskTitle = document.getElementById("task__title");
+let taskList = document.getElementById("task__title");
+let todoList = document.getElementById("todo_list")
+
+plus.addEventListener("click", ()=>{
+    taskTitle.style.display ="block"
+    let toDo = document.createElement("span");
+    toDo.setAttribute("id", "todo");
+    toDo.innerText = input.value;
+    todoList.append(toDo);
+    let remove = document.createElement("button");
+    remove.setAttribute("id", "remove");
+    remove.innerText = "X";
+    todoList.append(remove);
+    let brow =document.createElement("br");
+    todoList.append(brow);
+    input.value = "";
+
+    toDo.addEventListener("click", ()=>{
+        toDo.style.textDecorationLine = "line-through";
+        toDo.style.textDecorationColor = "white";
+    })
+
+    remove.addEventListener("click", ()=>{
+        remove.style.display = "none";
+        toDo.style.display = "none"
+    })
+})
